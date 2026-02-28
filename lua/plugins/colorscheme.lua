5488eb1b25eafd89cff99ab53d5bc58bfa310eb6
kar1221 <khyeong191@gmail.com>
2026-02-28 19:54:55 +0800

    remove some themes i dont use

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,35 +2,20 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night"
+      colorscheme = "onelight",
     },
+  },
+
+  {
+    "olimorris/onedarkpro.nvim",
   },
 
   {
     "AlexvZyl/nordic.nvim",
   },
 
-  { "EdenEast/nightfox.nvim" }, -- lazy
-
-  {
-    "zenbones-theme/zenbones.nvim",
-    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-    -- In Vim, compat mode is turned on as Lush only works in Neovim.
-    dependencies = "rktjmp/lush.nvim",
-    -- you can set set configuration options here
-    -- config = function()
-    --     vim.g.zenbones_darken_comments = 45
-    --     vim.cmd.colorscheme('zenbones')
-    -- end
-  },
-
   {
     "nyoom-engineering/oxocarbon.nvim",
-  },
-
-  {
-    "shaunsingh/nord.nvim",
   },
 
   {
@@ -50,41 +35,6 @@ return {
   },
 
   {
-    "marko-cerovac/material.nvim",
-    opts = {
-      plugins = { -- Uncomment the plugins that you use to highlight them
-        -- Available plugins:
-        "blink",
-        -- "coc",
-        -- "colorful-winsep",
-        "dap",
-        -- "dashboard",
-        -- "eyeliner",
-        -- "fidget",
-        -- "flash",
-        -- "gitsigns",
-        -- "harpoon",
-        -- "hop",
-        -- "illuminate",
-        -- "indent-blankline",
-        -- "lspsaga",
-        "mini",
-        -- "neo-tree",
-        -- "neogit",
-        -- "neorg",
-        -- "neotest",
-        -- "noice",
-        -- "nvim-cmp",
-        -- "nvim-navic",
-        -- "nvim-notify",
-        -- "nvim-tree",
-        "nvim-web-devicons",
-        -- "rainbow-delimiters",
-        -- "sneak",
-        -- "telescope",
-        -- "trouble",
-        "which-key",
-      },
-    },
+    "folke/tokyonight.nvim",
   },
 }
